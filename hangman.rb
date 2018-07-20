3ac478945299efc36f7e03c6f6e240b0adb66798
Calstream <calstream.lgn@gmail.com>
2018-07-21 00:00:40 +0300

    new index page with animation

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  "Hello bitch"
   erb :index, :locals => {}
+end
+
+get '/new_game' do
+  erb :new_game, :locals => {}
+end
+
+get '/game' do
+  erb :game, :locals => {}
 end
