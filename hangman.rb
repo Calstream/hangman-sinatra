b4dd2e0b9af7a52d19d4fa66931afdfebf4852cd
Calstream <calstream.lgn@gmail.com>
2018-07-21 23:55:54 +0300

    changing pages on click

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+word_length = 0
+
 get '/' do
   erb :index, :locals => {}
 end
 
-get '/new_game' do
-  erb :new_game, :locals => {}
+post '/settings' do
+  erb :settings, :locals => {:word_length => word_length}
 end
 
 get '/game' do
