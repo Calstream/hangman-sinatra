03a8d4278cb9f708aa966df467c5ec0bc1af9409
Calstream <calstream.lgn@gmail.com>
2018-07-22 17:49:45 +0300

    russian keyboard js

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,4 +1,4 @@
-function init()
+function init_en()
 {
   let keyboard = document.querySelector("#keyboard");
   let i = 0;
@@ -7,7 +7,8 @@ function init()
       let key = document.createElement("div");
       key.className = "key";
       key.className += " button";
-      let char = String.fromCharCode(97 + i).toUpperCase();
+      key.className += " en";
+      let char = String.fromCharCode("A".charCodeAt(0) + i).toUpperCase();
       key.id = char + "_key"
       key.innerHTML += char;
       key.onclick = function() {
@@ -19,4 +20,31 @@ function init()
   s.style.marginLeft = "45px"
 }
 
-init();
+function init_ru()
+{
+  let keyboard = document.querySelector("#keyboard");
+  let i = 0;
+  for (i = 0; i < 32; i++)
+    {
+      let key = document.createElement("div");
+      key.className = "key";
+      key.className += " button";
+      key.className += " ru";
+      let char = String.fromCharCode("А".charCodeAt(0) + i).toUpperCase();
+      key.id = "key_" + i;
+      key.innerHTML += char;
+      key.onclick = function() {
+        // TODO
+      }
+      keyboard.appendChild(key);
+    }
+  for(i = 0; i < 11; i++)
+  {
+    let top_row_key = document.querySelector("#key_" + i);
+    top_row_key.style.marginTop = "30px"
+  }
+  let ts = document.querySelector("#key_22");
+  ts.style.marginLeft = "45px"
+}
+
+init_ru();
